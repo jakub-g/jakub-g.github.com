89275b1213e42548aea7aa415e921a8f1cf57f24
jakub-g <jakub.g.opensource@gmail.com>
2015-01-19 13:32:34 +0100

    quirksmode[width]: refresh on scroll and resize

--- quirksmode/widthtest.js
+++ quirksmode/widthtest.js
@@ -3,18 +3,33 @@ var writeroot,
 	testLayer,
 	counter=0;
 
-function init() {
+var lipsumButtonHtml = '<input type=button onclick="removeLoremIpsum()" value="Remove Lorem Ipsum">';
+
+
+function removeLoremIpsum () {
+    document.getElementById("loremipsum").innerHTML = "";
+    refreshTest({type: "DOM elt removal"});
+}
+
+function init(evt) {
 	writeroot = document.getElementById('writeroot');
 	testEl = document.getElementById('testEl');
 	testLayer = document.getElementById('huge');
 	document.getElementById('testButton').onclick = refreshTest;
+	document.getElementById('removeLipsumButton').onclick = removeLoremIpsum;
+
 //	document.getElementById('stretch').onclick = stretchDiv;
-	refreshTest();
-	refreshTest(); //repeat so that the writeroot is filled with data at the moment its height is measured.
+
+	var lipsum = document.getElementById('loremipsum');
+	lipsum.innerHTML = lipsumButtonHtml + '<br>' + new Array(250).join('Lorem ipsum dolor sit amet. ');
+
+	refreshTest(evt);
+	refreshTest(evt); //repeat so that the writeroot is filled with data at the moment its height is measured.
 }
 
-function refreshTest() {
+function refreshTest(evt) {
 	log('Iteration ' + counter);
+	log('Last event: ' + (evt ? evt.type : ""));
 	counter +=1;
 	log('screen.width/height = ' + screen.width + ' / ' + screen.height); // + ' = ' + screen.width/screen.height);
 	log('documentElement.offset = ' + document.documentElement.offsetWidth + ' / ' + document.documentElement.offsetHeight);
@@ -40,10 +55,17 @@ function refreshTest() {
 var logString;
 
 function log(msg) {
-	logString += msg + '<br>';	
+	logString += msg + '<br>';
 }
 
 function set () {
+	// fixed positioning is not what we want on mobile when the page is zoomed, we have to emulate it width absolute
+	// pageX/YOffset is more reliable on IE11 mobile that (docEl / body).scrollLeft/Top when zoomed
+	var doc = document, docEl = document.documentElement;
+	var right = document.documentElement.clientWidth - (window.pageXOffset) - window.innerWidth;
+	writeroot.style.right = (right > 0 ? right : 0) + "px";
+	writeroot.style.top = (window.pageYOffset) + "px";
+
 	writeroot.innerHTML = logString;
 	logString = '';
 }
@@ -51,3 +73,5 @@ function set () {
 if (!window.onload) {
 	window.onload = init;
 }
+
+document.onscroll = window.onresize = refreshTest;
