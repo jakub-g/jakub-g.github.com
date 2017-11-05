86b5dce98fc607657ab3dc485f157bca3eca6c32
jakub-g <jakub.g.opensource@gmail.com>
2017-11-05 19:00:14 +0100

    disable ga

--- ga.js
+++ ga.js
@@ -1,7 +1,3 @@
-var _gaq = [];
-_gaq.push(['_setAccount', 'UA-36609750-1']);
-_gaq.push(['_trackPageview']);
-
 var sc_project=8533946, sc_invisible=1, sc_security="c702a775";
 
 var add=function(url) {
@@ -9,5 +5,4 @@ var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async
 ga.src = url;
 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 };
-add('https://www.google-analytics.com/ga.js');
 add('https://www.statcounter.com/counter/counter.js');
